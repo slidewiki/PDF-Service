eed32f225213b986b534db42b2627a97f1da1e51
urashidmir <umar.mir@open.ac.uk>
2017-08-21 11:47:34 +0100

    package.json updated

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -4,15 +4,15 @@ const co = require('../common');
 
 module.exports = {
   'deck': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'https://deckservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
   'pdf': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_PDF)) ? process.env.SERVICE_URL_PDF : 'https://pdfservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_PDF)) ? process.env.SERVICE_URL_PDF : 'http://pdfservice'
   },
   'user':{
-    uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'https://userservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'http://userservice'
   },
   'platform':{
-    uri: (!co.isEmpty(process.env.SERVICE_URL_PLATFORM)) ? process.env.SERVICE_URL_PLATFORM : 'https://platform.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_PLATFORM)) ? process.env.SERVICE_URL_PLATFORM : 'http://platform'
   }
 };
